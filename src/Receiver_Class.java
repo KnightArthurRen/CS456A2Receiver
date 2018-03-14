10d4a7914085c6750389d38260b938a4bb6e39b5
Arthur <renyirw@gmail.com>
2018-03-13 20:18:45 -0400

    Clean up

--- src/Receiver_Class.java
+++ src/Receiver_Class.java
@@ -8,9 +8,8 @@ import java.net.InetAddress;
  */
 public class Receiver_Class {
     private InetAddress emulator_ip;
-    private int emulator_port, reciver_port,last_seq;
+    private int emulator_port,last_seq;
     private DatagramSocket socket;
-    private DatagramPacket buffer;
     private packet received_packet;
     private FileWriter output,arrival;
 
@@ -33,7 +32,6 @@ public class Receiver_Class {
     }
     private void send_ack() {
         try {
-            System.out.println("start send ack");
             packet old_ack = packet.createACK(last_seq);
             try {
                 DatagramPacket binary = new DatagramPacket(old_ack.getUDPdata(), old_ack.getUDPdata().length,emulator_ip,emulator_port);
@@ -41,7 +39,6 @@ public class Receiver_Class {
             } catch (java.io.IOException e) {
                 System.err.println("Receiver_Class: cannot send old_ack!");
             }
-            System.out.println("ack send successful");
         } catch (java.lang.Exception e ) {
             System.err.println("Receiver_Class: cannot create old_ack package");
         }
@@ -51,9 +48,7 @@ public class Receiver_Class {
     public Receiver_Class(InetAddress emulator_ip, int emulator_port, int reciver_port, String output_file_name) {
         this.emulator_ip = emulator_ip;
         this.emulator_port = emulator_port;
-        this.reciver_port = reciver_port;
         this.last_seq = -1;
-        System.out.println("Constructor called");
 //        Build the socket
         try {
             socket = new DatagramSocket(reciver_port);
@@ -62,7 +57,7 @@ public class Receiver_Class {
         }
 
         byte [] buff = new byte[1000];
-        this.buffer = new DatagramPacket(buff,buff.length);
+        DatagramPacket buffer = new DatagramPacket(buff,buff.length);
 
 //        Create log + output file
         try {
@@ -77,7 +72,6 @@ public class Receiver_Class {
 //            Receive the package
             try {
                 socket.receive(buffer);
-                System.out.println("package received");
             } catch (java.io.IOException e) {
                 System.err.println("Receiver_Class: cannot receive package!");
             }
@@ -90,13 +84,11 @@ public class Receiver_Class {
 //            Check the package
 //            Check if it's EOT
             if(received_packet.getType() == 2) {
-                System.out.println("EOT received");
                 try{
                     packet eot_ack = packet.createEOT(received_packet.getSeqNum());
                     DatagramPacket binary = new DatagramPacket(eot_ack.getUDPdata(), eot_ack.getUDPdata().length,emulator_ip,emulator_port);
                     try {
                         socket.send(binary);
-                        System.out.println("EOT send!!!");
                     } catch (java.io.IOException e) {
                         System.err.println("Receiver_Class: failed to send EOT");
                     }
@@ -111,27 +103,19 @@ public class Receiver_Class {
 //            If the first package is wrong, no ack is received
 //            If the first package is correct, update seq and log
             if(last_seq == -1 && received_packet.getSeqNum() != 0) {
-                System.out.println("First pacakge has problem!!!!");
                 continue;
             }
             if(last_seq +1 % 32 != received_packet.getSeqNum()) {
 //                If not expected package, drop it and send ack of last package
-                System.out.println("duplicate, resend ack");
-                System.out.print("received packet is ");
-                System.out.println(received_packet.getSeqNum());
-                System.out.print("Expected packet is");
-                System.out.println(last_seq +1 % 32);
                 log(received_packet.getSeqNum());
                 send_ack();
             } else {
 //                If it's expected, write down the data, update seq and send ack
 //                Write down data
-                System.out.println("received regular pacakge");
                 log(received_packet.getSeqNum());
                 save_data( new String(received_packet.getData()));
                 last_seq = received_packet.getSeqNum();
                 send_ack();
-                System.out.println("Ack send");
             }
         }
 //        After finished, close the log file writers
